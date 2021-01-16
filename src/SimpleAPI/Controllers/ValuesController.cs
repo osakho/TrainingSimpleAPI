a7f3e5a54029e60f0f298a7d99a27757ac823b27
Sakho <csakho@hotmail.com>
2021-01-17 00:39:28 +0100

    changed return values

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" ,"value2"};
+            return new string[] { "Training", "Deployment" ,"Azure-Devops"};
         }
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
